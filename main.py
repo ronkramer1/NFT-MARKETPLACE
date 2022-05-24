1e4c5cd819eb069f275a2aaaab0118cb10287ded
Ron Kramer <ronkramer44@gmail.com>
2022-05-24 12:28:13 +0300

    updated main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from select import select
+
 from Crypto.PublicKey import ECC
 
+from block import Block
+from peer import Peer
+from transaction import Transaction
 from utils import *
 from wallet import Wallet
+
+from PyQt5 import QtCore as qtc
 
 
 class Main:
 
     def __init__(self):
         self.wallet = None
+        self.peer = Peer()
 
     def create_wallet2(self):
         self.wallet = Wallet()
@@ -59,6 +67,107 @@ class Main:
             print("there is no wallet on this device.")
 
         return self.wallet
+
+    def constant_receive(self):
+        """tries to receive messages from other peers (both tcp and udp), calls itself every 0.1 seconds"""
+        if self.peer.tcp_client:
+            rlist, wlist, xlist = select([self.peer.udp_receiver, self.peer.tcp_client], [], [], 0.01)
+        else:
+            rlist, wlist, xlist = select([self.peer.udp_receiver], [], [], 0.01)
+        for sock in rlist:
+            if sock == self.peer.udp_receiver:
+                received_message = self.peer.udp_receive()
+                self.received_from_udp_socket(received_message)
+
+            if sock == self.peer.tcp_client:
+                try:
+                    received_message = sock.recv(RECV_SIZE).decode()
+                except ConnectionResetError:
+                    received_message = ''
+                if received_message[:len("position ")] == "position ":
+                    block_position_from_end_of_chain_to_send = int(received_message[len("position "):])
+                    self.send_a_missing_block(block_position_from_end_of_chain_to_send)
+                elif received_message[len("finished"):] == "finished" or received_message == '':
+                    self.peer.close_client()
+
+        qtc.QTimer.singleShot(100, self.constant_receive)
+
+    def received_from_udp_socket(self, message):
+        """handles a message that was received by the udp receiver socket"""
+        if type(message) == Transaction:
+            if self.wallet.add_transaction_to_pool(message):
+                self.add_transaction_to_pool_tree(message)
+        elif type(message) == Block:
+            self.wallet.add_proposed_block(message)
+            self.add_block_to_proposed_tree(message)
+        elif type(message) == str and message[:len("connected")] == "connected":
+            self.send_a_missing_block(self.wallet.blockchain.chain[-1].block_number)
+        else:
+            print(message)
+
+    def send_a_missing_block(self, position):
+        """sends a block that might be missing for a newly connected peer on tcp"""
+        block_to_send = [block for block in self.wallet.blockchain.chain if block.block_number == position][0]
+        self.peer.tcp_client_send(block_to_send)
+
+    def request_missing_blocks(self):
+        """starts requesting blocks that might be missing"""
+        self.collect_blocks()
+
+    def collect_blocks(self):
+        """collects blocks that might be missing from other peers"""
+        missing_blocks_by_peer = {}
+        self.peer.request_update_connection()
+        finished_so_far = 0
+        tcp_connected_peers = []
+        finished_collecting_missing_blocks = False
+
+        def collect_blocks_networking():
+            nonlocal finished_so_far
+            nonlocal tcp_connected_peers
+            nonlocal finished_collecting_missing_blocks
+
+            rlist, wlist, xlist = [], [], []
+            if len(tcp_connected_peers) <= NUMBER_OF_CONNECTED_CLIENTS:
+                rlist, wlist, xlist = select([self.peer.tcp_server] + tcp_connected_peers, [], [], 0.01)
+            for sock in rlist:
+                if sock == self.peer.tcp_server:
+                    (new_sock, address) = self.peer.tcp_server.accept()
+                    tcp_connected_peers.append(new_sock)
+
+                elif sock in tcp_connected_peers:
+                    received_message = sock.recv(RECV_SIZE).decode()
+                    if received_message[:len("Block: ")] == "Block: ":
+                        received_message = received_message[len("Block: "):]
+                        received_block = Block.deserialize(received_message)
+                        if received_block.block_number not in [block.block_number for block in self.wallet.blockchain.chain]:  # if block number isn't alewady in chain
+                            if sock not in missing_blocks_by_peer:
+                                missing_blocks_by_peer[sock] = []
+                            missing_blocks_by_peer[sock].append(received_block)
+                            sock.send(f"position {received_block.block_number - 1}".encode('utf-8'))
+                        else:
+                            sock.send("finished".encode('utf-8'))
+                            finished_so_far += 1
+                            if finished_so_far >= NUMBER_OF_CONNECTED_CLIENTS:
+                                finished_collecting_missing_blocks = True
+
+                    elif received_message == '':
+                        tcp_connected_peers.remove(sock)
+
+                    else:
+                        print(received_message)
+
+            if not (self.finished_collecting_missing_blocks_by_button or finished_collecting_missing_blocks):
+                qtc.QTimer.singleShot(1000, collect_blocks_networking)
+            else:
+                tcp_connected_peers = []
+                self.peer.close_server()
+                if finished_collecting_missing_blocks:
+                    self.handle_collected_blocks(list(missing_blocks_by_peer.values()))
+                self.finish_entering_wallet()
+
+        collect_blocks_networking()
+
 
 
 if __name__ == "__main__":
